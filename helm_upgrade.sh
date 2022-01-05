75605f3e13dbb6b778626332011cffa544af5438
Georvic Tur <alexanderstower@gmail.com>
2022-01-05 23:36:05 +0100

    jenkins password is now passed through input args

--- helm_upgrade.sh
+++ helm_upgrade.sh
@@ -5,4 +5,5 @@ helm upgrade \
     --set volumes.jenkins.local.path="$( pwd )/volumes/jenkins_home" \
     --set volumes.mlflow.local.path="$( pwd )/volumes/mlflow" \
     --set volumes.elastic.local.path="$( pwd )/volumes/elastic_data" \
+    --set secrets.jenkins.jenkins_admin_password="my53cur3p455w.rd" \
     cd4ml ./chart
