3b05281dede2d432ef26cac85b3c572b73ca56d0
Georvic Tur <alexanderstower@gmail.com>
2022-01-05 04:32:50 +0100

    volume added for jenkins in the helm chart. Dockerfile modified for jenkins: it uses the cli for installing plugins now

--- helm_upgrade.sh
+++ helm_upgrade.sh
@@ -2,4 +2,5 @@ helm upgrade \
     --install -f ./chart/values.yaml \
     --namespace cd4ml \
     --set volumes.minio.local.path="$( pwd )/data" \
+    --set volumes.jenkins.local.path="$( pwd )/jenkins_home" \
     cd4ml ./chart
