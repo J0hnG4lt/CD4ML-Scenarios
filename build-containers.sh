ec2e7498542bc066c1f5b4ac66c1cc68d07265ad
Georvic Tur <alexanderstower@gmail.com>
2022-01-05 02:23:23 +0100

    Jenkins container had a problem with the git plugin

--- build-containers.sh
+++ build-containers.sh
@@ -1,21 +1,24 @@
+# Fail fast on any error
+# set -e
+
 # Build all of the containers
 
 echo "Building Jenkins Master"
-docker build --rm -f Dockerfile-jenkins -t ericnaglertw/cd4ml-build-master:2 .
+docker build --rm -f Dockerfile-jenkins -t georvic/cd4ml-build-master:2 .
 echo "Building MLFlow"
-docker build --rm -f Dockerfile-mlflow -t ericnaglertw/cd4ml-mlflow:1 .
+docker build --rm -f Dockerfile-mlflow -t georvic/cd4ml-mlflow:1 .
 echo "Building Model Server"
-docker build --rm -f Dockerfile-model -t ericnaglertw/cd4ml-model-server:1 .
+docker build --rm -f Dockerfile-model -t georvic/cd4ml-model-server:1 .
 
 echo "Building Fluentd"
 pushd .
 cd fluentd
-docker build --rm  -f Dockerfile -t ericnaglertw/cd4ml-fluentd:1 .
+docker build --rm  -f Dockerfile -t georvic/cd4ml-fluentd:1 .
 popd
 
 # Push to DockerHub
 
-# docker push ericnaglertw/cd4ml-build-master:2
-# docker push ericnaglertw/cd4ml-mlflow:1
-# docker push ericnaglertw/cd4ml-fluentd:1
-# docker push ericnaglertw/cd4ml-model-server:1
+docker push georvic/cd4ml-build-master:2
+docker push georvic/cd4ml-mlflow:1
+docker push georvic/cd4ml-fluentd:1
+docker push georvic/cd4ml-model-server:1
